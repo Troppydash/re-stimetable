7b8b5af30abef70b1f5fe59b825dfe01a18c8b11
Terry Qi <fortianrui4@gmail.com>
2021-07-19 01:24:32 +1200

    initial bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div id="nav">
+        <div class="st-text">Hello World</div>
+        <router-link to="/">Home</router-link>
+        |
+        <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// the custom stylesheet here
+@import "../node_modules/stimetable.css/stimetable.css";
+
+// general styles
+html,
+body {
+    margin: 0;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+    transition-delay: 100ms;
+    transition-duration: 100ms;
 }
+
 </style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    pwa: {
+        name: "Re-STimetable",
+        themeColor: "#b82832",
+        msTileColor: '#b82832',
+        appleMobileWebAppCapable: 'yes',
+        manifestOptions: {
+            start_url: '/timetable',
+        }
+    }
+}
